827131d0939295024c6638fb2c6430fdb9e7c5a0
Romeo Orsolino <romeo.orsolino@iit.it>
2019-02-28 15:18:30 +0100

    hyq can make a circle for foot

--- hyq.py
+++ hyq.py
@@ -5,13 +5,13 @@ from datetime import datetime
 import time
 
 p.connect(p.GUI)
-plane = p.loadURDF("plane.urdf")
+plane = p.loadURDF("plane.urdf", [0, 0, -0.5])
 p.setGravity(0, 0, -9.8)
 p.setTimeStep(1. / 500)
 # p.setDefaultContactERP(0)
 # urdfFlags = p.URDF_USE_SELF_COLLISION+p.URDF_USE_SELF_COLLISION_EXCLUDE_ALL_PARENTS
 urdfFlags = p.URDF_USE_SELF_COLLISION
-quadruped = p.loadURDF("hyq/hyq.urdf", [0, 0, 0.5], [0, 0.5, 0.5, 0], flags=urdfFlags, useFixedBase=False)
+quadruped = p.loadURDF("hyq/hyq.urdf", [0, 0, 0], [0, 0.5, 0.5, 0], flags=urdfFlags, useFixedBase=False)
 
 my_chain = Chain.from_urdf_file("data/hyq/hyq.urdf")
 
@@ -91,7 +91,8 @@ for j in range(p.getNumJoints(quadruped)):
 
 p.setRealTimeSimulation(1)
 
-sawyerEndEffectorIndex = 3
+sawyerEndEffectorIndex = [3, 7, 11, 15]
+cyrcle_center = [0.2, -0.2, 0.2, -0.2, 0.3, 0.3, -0.3, -0.3]
 numJoints = p.getNumJoints(quadruped)
 jd=[0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001]
 useRealTimeSimulation = 0
@@ -116,12 +117,16 @@ while (1):
         dt = datetime.now()
         t = (dt.second / 60.) * 2. * math.pi
     else:
-        t = t + 0.01
+        t += 0.01
         time.sleep(0.01)
 
-    for i in range(1):
-        pos = [0.2 + 0.1 * math.cos(t), 0.2 + 0.1 * math.sin(t), 0.05]
-        jointPoses = p.calculateInverseKinematics(quadruped, sawyerEndEffectorIndex, pos, jointDamping=jd)
+
+    for i in range(4):
+        if orient[2] >= 0.2:
+            pos = [body_xyz[0] + cyrcle_center[i] + 0.1 * math.cos(t), body_xyz[1] + cyrcle_center[i+4] + 0.1 * math.sin(t), - 0.5]
+        else:
+            pos = [body_xyz[0] - cyrcle_center[i] + 0.1 * math.cos(t), body_xyz[1] - cyrcle_center[i+4] + 0.1 * math.sin(t), - 0.5]
+        jointPoses = p.calculateInverseKinematics(quadruped, sawyerEndEffectorIndex[i], pos, jointDamping=jd)
 
         # reset the joint state (ignoring all dynamics, not recommended to use during simulation)
         for i in range(numJoints):
@@ -130,11 +135,10 @@ while (1):
             if qIndex > -1:
                 p.resetJointState(quadruped, i, jointPoses[qIndex - 7])
 
-    ls = p.getLinkState(quadruped, sawyerEndEffectorIndex)
-    if (hasPrevPose):
-        p.addUserDebugLine(prevPose, pos, [0, 0, 0.3], 1, trailDuration)
-        p.addUserDebugLine(prevPose1, ls[4], [1, 0, 0], 1, trailDuration)
-    prevPose = pos
-    prevPose1 = ls[4]
-    hasPrevPose = 1
-
+#        ls = p.getLinkState(quadruped, sawyerEndEffectorIndex[i])
+#        if (hasPrevPose):
+#            p.addUserDebugLine(prevPose, pos, [0, 0, 0.3], 1, trailDuration)
+#            p.addUserDebugLine(prevPose1, ls[4], [1, 0, 0], 1, trailDuration)
+#        prevPose = pos
+#        prevPose1 = ls[4]
+#        hasPrevPose = 1
